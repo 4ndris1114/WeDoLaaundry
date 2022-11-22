e696ae2bbae7b0ab5ec35dd2db9a6ba3f5fdc304
4ndris1114 <10407531@ucn.dk>
2022-11-22 16:37:46 +0100

    Customer table script modification
    Added "ON DELETE" and "ON CASCADE" rules

--- SQL/Customer sql script.sql
+++ SQL/Customer sql script.sql
@@ -4,16 +4,16 @@ DROP TABLE IF EXISTS Customer
 
 CREATE TABLE Customer (
 id int PRIMARY KEY IDENTITY (1000,1) NOT NULL,
-fname varchar(40) NOT NULL,
-lname varchar(40) NOT NULL,
-phone varchar(15) NOT NULL,
+fname varchar(50) NOT NULL,
+lname varchar(50) NOT NULL,
+phone varchar(20) NOT NULL,
 postalCode int NOT NULL,
 city varchar(100) NOT NULL,
 [address] varchar(100) NOT NULL,
-email varchar(50) NOT NULL,
-userId nvarchar(450) NOT NULL,
+email nvarchar(256) NOT NULL,
+userId nvarchar(450),
 userType varchar(40) NOT NULL,
 
 CONSTRAINT FK_CustomerUser 
-FOREIGN KEY (userId) REFERENCES AspNetUsers(id)
+FOREIGN KEY (userId) REFERENCES AspNetUsers(Id) ON DELETE SET NULL ON UPDATE CASCADE
 )
