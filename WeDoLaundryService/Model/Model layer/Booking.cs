686e2bcd715bccdf1eb288c24c671d4fa23f4dcd
Gabriel Pędlowski <93519015+g-ped@users.noreply.github.com>
2022-11-23 12:02:05 +0100

    booking model, dto and model convertion

---
+++ WeDoLaundryService/Model/Model layer/Booking.cs
@@ -0,0 +1,63 @@
+﻿using Data.Model_layer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Model_layer
+{
+    public class Booking
+    {
+        public enum Status 
+        {
+            BOOKED = 0,
+            IN_PROGRESS = 1,
+            RETURNED = 2
+        }
+
+
+        public int Id { get; set; }
+        public Customer Customer { get; set; }
+        public int DriverId { get; set; }
+        public DateTime PickUpTime { get; set; }
+        public DateTime ReturnTime { get; set; }
+        public string PickUpAddress { get; set; }
+        public string ReturnAddress { get; set; }
+        public Status BookingStatus { get; set; }
+        public int AmountOfBags { get; set; }
+        public int InvoiceId { get; set; }
+
+        public Booking(int id, Customer customer, int driverId, DateTime pickUpTime, DateTime returnTime, string pickUpAddress, string returnAddress, Status bookingStatus, int amountOfBags, int invoiceId)
+        {
+            Id = id;
+            Customer = customer;
+            DriverId = driverId;
+            PickUpTime = pickUpTime;
+            ReturnTime = returnTime;
+            PickUpAddress = pickUpAddress;
+            ReturnAddress = returnAddress;
+            BookingStatus = bookingStatus;
+            AmountOfBags = amountOfBags;
+            InvoiceId = invoiceId;
+        }
+
+        public Booking(Customer customer, int driverId, DateTime pickUpTime, DateTime returnTime, string pickUpAddress, string returnAddress, Status bookingStatus, int amountOfBags, int invoiceId)
+        {
+            Customer = customer;
+            DriverId = driverId;
+            PickUpTime = pickUpTime;
+            ReturnTime = returnTime;
+            PickUpAddress = pickUpAddress;
+            ReturnAddress = returnAddress;
+            BookingStatus = bookingStatus;
+            AmountOfBags = amountOfBags;
+            InvoiceId = invoiceId;
+        }
+
+        public Booking()
+        {
+
+        }
+    }
+}
---
+++ WeDoLaundryService/Service/DTOs/BookingReadDTO.cs
@@ -0,0 +1,40 @@
+﻿using Data.Model_layer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using static Model.Model_layer.Booking;
+
+namespace Service.DTOs
+{
+    public class BookingReadDTO
+    {
+        public int Id { get; set; }
+        public Customer Customer { get; set; }
+        public int DriverId { get; set; }
+        public DateTime PickUpTime { get; set; }
+        public DateTime ReturnTime { get; set; }
+        public string PickUpAddress { get; set; }
+        public string ReturnAddress { get; set; }
+        public Status BookingStatus { get; set; }
+        public int AmountOfBags { get; set; }
+        public int InvoiceId { get; set; }
+
+        public BookingReadDTO()
+        {
+
+        }
+
+        public BookingReadDTO(int id, Customer customer, int driverId, DateTime pickUpTime, DateTime returnTime, string pickUpAddress, string returnAddress, Status bookingStatus, int amountOfBags, int invoiceId)
+        {
+            Id = id;
+            Customer = customer;
+            DriverId = driverId;
+            PickUpTime = pickUpTime;
+            ReturnTime = returnTime;
+            PickUpAddress = pickUpAddress;
+            ReturnAddress = returnAddress;
+            BookingStatus = bookingStatus;
+            AmountOfBags = amountOfBags;
+            InvoiceId = invoiceId;
+        }
+    }
+}
