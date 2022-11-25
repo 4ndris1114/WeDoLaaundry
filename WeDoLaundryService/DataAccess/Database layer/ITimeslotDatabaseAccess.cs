6ca83bf7b91873dfa48dd2dd23ae59fd31af5801
4ndris1114 <10407531@ucn.dk>
2022-11-25 13:31:48 +0100

    Create ITimeslotDatabaseAccess.cs

---
+++ WeDoLaundryService/DataAccess/Database layer/ITimeslotDatabaseAccess.cs
@@ -0,0 +1,10 @@
+﻿using Model.Model_layer;
+
+namespace DataAccess.Database_layer
+{
+    public interface ITimeslotDatabaseAccess
+    {
+        bool IncreaseAvailiability();
+        List<TimeSlot> GetAll();
+    }
+}
