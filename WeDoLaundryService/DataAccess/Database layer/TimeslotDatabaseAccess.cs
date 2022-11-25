0abef98e76849748dff2ed52af3f94e5cee968d5
4ndris1114 <10407531@ucn.dk>
2022-11-25 13:25:11 +0100

    timeslot dataaccess class

---
+++ WeDoLaundryService/DataAccess/Database layer/TimeslotDatabaseAccess.cs
@@ -0,0 +1,88 @@
+﻿using Data.Database_layer;
+using Data.Model_layer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualBasic;
+using Model.Model_layer;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Model.Model_layer.Booking;
+
+namespace DataAccess.Database_layer
+{
+    public class TimeslotDatabaseAccess
+    {
+        private readonly string _connectionString;
+        private readonly ICustomerAccess _customerAccess;
+
+        public TimeslotDatabaseAccess(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("WeDoLaundry");
+            _customerAccess = new CustomerDatabaseAccess(configuration);
+        }
+
+        public TimeslotDatabaseAccess(string connectionString)
+        {
+            _connectionString = connectionString;
+            _customerAccess = new CustomerDatabaseAccess(_connectionString);
+        }
+
+        public bool IncreaseAvailability(DateOnly date, string timeSlot)
+        {
+            int numberOfRowsModified = 0;
+            string SQL_string = "UPDATE TimeSlots SET [availability] = [availability] - 1 WHERE date = @Date AND slot = @Slot";
+            using (SqlConnection con = new(_connectionString))
+            using (SqlCommand command = new(SQL_string, con))
+            {
+                SqlParameter dateParam = new("@Date", date);
+                command.Parameters.Add(dateParam);
+                SqlParameter slotParam = new("@Slot", timeSlot);
+                command.Parameters.Add(slotParam);
+
+                con.Open();
+
+                numberOfRowsModified = command.ExecuteNonQuery();
+
+                con.Close();
+            }
+
+            return (numberOfRowsModified > 0);
+        }
+
+        public List<TimeSlot> GetAll()
+        {
+            List<TimeSlot>? returnList = new();
+
+            string SQL_string = "SELECT * FROM TimeSlots";
+            using (SqlConnection con = new(_connectionString))
+            using (SqlCommand command = new(SQL_string, con))
+            {
+                con.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                returnList = new();
+                while (reader.Read())
+                {
+                    returnList.Add(GetTimeslotReader(reader));
+                }
+                con.Close();
+            }
+            return returnList;
+        }
+
+        private TimeSlot GetTimeslotReader(SqlDataReader reader)
+        {
+            TimeSlot returnTimeslot;
+            DateOnly date = DateOnly.Parse(reader.GetString(reader.GetOrdinal("date")));
+            string slot = reader.GetString(reader.GetOrdinal("slot"));
+            int availability = reader.GetInt32(reader.GetOrdinal("availability"));
+
+            returnTimeslot = new(date, slot, availability);
+
+            return returnTimeslot;
+        }
+
+    }
+}
