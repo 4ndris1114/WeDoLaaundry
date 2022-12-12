1b8f1d3b23ea3e0288ecbf03eb7484d55ef7aa54
Gabriel Pędlowski <93519015+g-ped@users.noreply.github.com>
2022-12-12 21:51:32 +0100

    something about RUD operations on customers

--- WeDoLaundryDesktopClient/WpfApp1/Views/CustomerListView.xaml.cs
+++ WeDoLaundryDesktopClient/WpfApp1/Views/CustomerListView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,38 @@ namespace WpfApp1.Views
         public CustomerListView()
         {
             InitializeComponent();
+        }
+
+        private void update_btn_Click(object sender, RoutedEventArgs e)
+        {
+            // missing a logic for update
+        }
+
+        private void delete_btn_Click(object sender, RoutedEventArgs e)
+        {
+            // missing a logic for delete
+
+        }
+
+        private void customersDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DataGrid dg = (DataGrid)sender;
+            DataRowView dr = (DataRowView)dg.SelectedItem; 
+
+            if (dr != null)
+            {
+                first_name_txt.Text = dr["FirstName"].ToString();
+                last_name_txt.Text = dr["LastName"].ToString();
+                phone_number_txt.Text = dr["Phone"].ToString();
+                email_txt.Text = dr["Email"].ToString();
+                postal_code_txt.Text = dr["PostalCode"].ToString();
+                city_txt.Text = dr["City"].ToString();
+                address_txt.Text = dr["Address"].ToString();
+                subscription_txt.Text = dr["CustomerType"].ToString();
+
+                update_btn.IsEnabled = true;
+                delete_btn.IsEnabled = true;
+            }
         }
     }
 }
