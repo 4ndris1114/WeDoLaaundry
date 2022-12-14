314ff72ffdde495d5704d9a2003536000c30bd7f
4ndris1114 <10407531@ucn.dk>
2022-12-14 13:28:12 +0100

    the rest

--- WeDoLaundryDesktopClient/WpfApp1/MainWindow.xaml.cs
+++ WeDoLaundryDesktopClient/WpfApp1/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GoogleAPI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace WpfApp1
         public MainWindow()
         {
             InitializeComponent();
+            ApiHelper.InitializeClient();
         }
     }
 }
